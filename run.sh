66a2d45c88f6a9b650edf1320ac156932b98a74f
Vladislav Feofilaktov <vfeofilaktov@ptsecurity.com>
2024-11-08 22:28:11 +0300

    Extract defines from run.sh to the cli arguments

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@
 
 sleep 2;
 
-(gcc -std=c11 -DSTELLA_DEBUG -DSTELLA_GC_STATS -DSTELLA_RUNTIME_STATS out.c stella/runtime.c stella/gc.c -o a.out) &&./a.out
+(gcc -std=c11 $2 out.c stella/runtime.c stella/gc.c -o a.out) &&./a.out
